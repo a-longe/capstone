67b32571531944b02899b1158be60e030047e687
Alex Longe <alex.m.longe@gmail.com>
2023-09-26 17:14:35 -0700

    started using chess-board to visualize chess board

--- python-chess/pySimpleGUITest.py
+++ python-chess/pySimpleGUITest.py
@@ -1,17 +1,14 @@
-import PySimpleGUI as gui                        
-# Define the window's contents
-layout = [  [gui.Text("Chess")],
-            [gui.Column([[0],[1],[2]])]
-          
-         ]     
-# Create the window
-window = gui.Window('Window Title', layout)      
-                                                
-# Display and interact with the Window
-event, values = window.read()                   # Event loop or Window.read call
+from chessboard import display
 
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
-# Finish up by removing from the screen
-window.close()                                  
+game_board = display.start()
+
+while True:
+    display.check_for_quit()
+    display.update(fen, game_board)
+
+    # board flip interface
+    if not game_board.flipped:
+        display.flip(game_board)
+    fen = input("fen: ")
