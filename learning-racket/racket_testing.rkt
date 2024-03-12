72982a9d96bd36728fa56c7a59b8a64273dab347
Alex Longe <alex.m.longe@gmail.com>
2024-03-12 14:11:30 -0700

    made my own unit tester, with a sucess message to look good

--- learning-racket/racket_testing.rkt
+++ learning-racket/racket_testing.rkt
@@ -8,4 +8,16 @@
 (define (tock x) 
   (+ x 3))
 
-(check-eq? (tock 23) 24 "tock func does not increment by 3")
+(define (a-check input expected msg)
+  (cond
+    [(equal? input expected) (display ".")]
+    [else (check-eq? input expected msg)])
+)
+
+(a-check (tock 23) 24 "will return false")
+(a-check (tock 23) 26 "will return true")
+(a-check (tock 23) 26 "will return true")
+(a-check (tock 23) 26 "will return true")
+(a-check (tock 23) 26 "will return true")
+
+;(check-eq? (tock 23) 24 "tock func does not increment by 3")
