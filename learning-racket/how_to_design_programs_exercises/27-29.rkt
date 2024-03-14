364f53c9a31406958560c14e70bbc53ac34d970a
Alex Longe <alex.m.longe@gmail.com>
2024-03-14 13:48:21 -0700

    small work on 27-29 exc.

--- learning-racket/how_to_design_programs_exercises/27-29.rkt
+++ learning-racket/how_to_design_programs_exercises/27-29.rkt
@@ -9,23 +9,37 @@
 ;; pixels at coordinates that corespond to the procedure
 ;; Procedure must return a number
 (require 2htdp/image)
-(define (graph-img proc size)
+(define (num-func->img proc size)
   (for/fold ([img (rectangle size size "solid" "gray")])
             ([x-value (in-range (* (quotient size 2) -1) (quotient size 2) 0.1)])
     (place-image (square 2 "solid" "black")
                  (+ x-value (quotient size 2))
-                 (+ (* (disp x-value) -1) (quotient size 2))
+                 (+ (* (proc x-value) -1) (quotient size 2))
                  img)
     ))
 
 (define (disp x)
-  (expt x 2))
+  (abs x))
 
-(graph-img disp 500)
 ;; NOTES after completing:
 ;; probably the 'nicer' way of doing it is by recursion where the
 ;; result of the previous is passed into the next recusion but
 ;; using rackets build in way of storing state between loops seemed easier
 
 ;; 27
+(define (avg-attendance price)
+  (- 120 (* (/ price 0.10) 15)))
+
+(define (revenue price)
+  (* (avg-attendance price) price))
+
+(define (cost price)
+  (+ 180 (* 0.04 (avg-attendance price))))
+
+(define (profit price)
+  (- (revenue price)
+     (cost price)))
+
+
+(num-func->img profit 400)
 
